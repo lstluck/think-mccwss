f613e6bf28fb0bb096a62028b2c152fe7ce665ea
CWAOST <github@890413.xyz>
2024-07-05 16:17:38 +0800

    d

---
+++ src/Service.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace mccwss;
+
+use mccwss\command\WebSocketServer as ServerCommand;
+class Service extends \think\Service
+{
+
+    public function boot()
+    {
+
+        $this->commands(
+            ServerCommand::class
+        );
+    }
+
+}
